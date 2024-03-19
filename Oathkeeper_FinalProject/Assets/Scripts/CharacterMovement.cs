1977de90d950f6f30af3ec94b7c5cc92d2edc4ae
Symono <51352256+Symono@users.noreply.github.com>
2024-03-19 17:07:55 -0500

    deleted old code

--- Oathkeeper_FinalProject/Assets/Scripts/CharacterMovement.cs
+++ Oathkeeper_FinalProject/Assets/Scripts/CharacterMovement.cs
@@ -67,7 +67,6 @@ public class Movement : MonoBehaviour
             Debug.Log("Collided with an enemy");
             // load into battle scene
             SceneManager.LoadScene("BattleScene");
-            
         }
     }
 
